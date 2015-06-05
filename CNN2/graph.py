625fe42cadc6ddfaaf0f4366e2c6f7ae2b66eeed
hessenh <hansolavhessen@gmail.com>
2015-06-04 17:44:56 -0700

    graph

---
+++ CNN2/graph.py
@@ -0,0 +1,20 @@
+from numpy import *
+import math
+import matplotlib.pyplot as plt
+
+
+
+#4-20-100-10
+i = [5,10,30,60]
+a = [0.87,0.89,0.936,0.942]
+plt.plot(i,a,'r')
+
+
+#6-20-50-10
+i = [5,10,30,60]
+a = [0.57,0.894,0.9450,0.956]
+plt.plot(i,a,'b')
+
+
+
+plt.show()
